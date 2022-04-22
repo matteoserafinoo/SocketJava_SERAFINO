df1b4ab1904d8273c60b27deabf30b3500d4b5ec
matteoserafinoo <matteoserafino8@gmail.com>
2022-04-22 14:26:56 +0200

    22_04_secondo_commit

--- src/socketjava_serafino/Server.java
+++ src/socketjava_serafino/Server.java
@@ -5,8 +5,13 @@
  */
 package socketjava_serafino;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,6 +21,9 @@ import java.util.logging.Logger;
  */
 public class Server {
         ServerSocket ss;
+        Socket so;
+        BufferedWriter bw;
+        BufferedReader br;
     public Server(int porta) {
             try {
                 ss = new ServerSocket(porta);
@@ -25,7 +33,40 @@ public class Server {
             }
          
     }
-
+    
+    public void ascolto()
+    {
+            try {
+                so = ss.accept();
+                System.out.println("Connessione stabilita");
+                br = new BufferedReader(new InputStreamReader(so.getInputStream()));
+                bw = new BufferedWriter(new OutputStreamWriter(so.getOutputStream()));
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    
+    public void scrivi(String messaggio)
+    {
+            try {
+                bw.write(messaggio+"\n");
+                bw.flush();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+    }
+    
+    public String leggi()
+    {
+        
+            try {
+                return br.readLine();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                return "Errore";
+            }
+    }
    
     
 }
